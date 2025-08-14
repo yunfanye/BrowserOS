b8ba18946e683e86f1b48e824f9eb681bf310c08
Nikhil Sonti <nikhilsv92@gmail.com>
2025-08-14 10:24:32 -0700

    cleaned-up old legacy side-panel

--- src/sidepanel/hooks/index.ts
+++ src/sidepanel/hooks/index.ts
@@ -1,6 +1,2 @@
-// Side panel state management
-export { useSidePanelState } from './useSidePanelState'
-export type { SidePanelState } from './useSidePanelState'
-
 // Side panel port messaging
 export { useSidePanelPortMessaging } from './useSidePanelPortMessaging' 
--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SidePanelPage } from './pages/SidePanelPage'
-import { App as AppV2 } from './v2/App'
-
-// Feature flag for v2 sidepanel
-const USE_V2 = true;
-
-// Log which version is being used
-console.log(`[Sidepanel] Using ${USE_V2 ? 'V2' : 'V1'} implementation`)
+import { App } from './v2/App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    {USE_V2 ? <AppV2 /> : <SidePanelPage />}
+    <App />
   </React.StrictMode>
 ) 
